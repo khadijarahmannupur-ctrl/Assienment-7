b5c7950da0fbedbc2e3b6ec00b0ff301cdab1ff6
khadijarahmannupur-ctrl <khadijarahmannupur@gmail.com>
2026-04-15 06:37:07 +0600

    navbar responsive perfectly

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -14,28 +14,37 @@ const Navbar = () => {
     </>
     return (
         <nav className='border-b border-gray-200'>
-            <div className="navbar bg-base-100 flex justify-between items-center container mx-auto">
-                <div className="navbar-start">
+            <div className="navbar bg-base-100 container mx-auto flex justify-between items-center">
+
+                {/* 🔹 Left side (mobile dropdown only) */}
+                <div className="md:hidden">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        <div tabIndex={0} role="button" className="btn btn-ghost">
+                            ☰
                         </div>
                         <ul
-                            tabIndex="-1"
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow"
+                        >
                             {links}
                         </ul>
                     </div>
-                    <img src={logo} alt="" />
                 </div>
-                <div className="navbar-center hidden lg:flex">
+
+                {/* 🔹 Logo */}
+                <div className="flex-1 flex justify-end md:justify-start">
+                    <img src={logo} alt="logo" className="h-7" />
+                </div>
+
+                {/* 🔹 Desktop Menu */}
+                <div className="hidden md:flex">
                     <ul className="menu menu-horizontal px-1">
                         {links}
                     </ul>
                 </div>
+
             </div>
         </nav>
-
     );
 };
 
